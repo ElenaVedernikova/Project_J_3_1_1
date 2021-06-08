4e92ed1c78937a74b7aa8c767c2dfc2eff62e7cd
ElenaVedernikova <lenok-v@live.ru>
2021-06-08 18:37:21 +0300

    “commit”

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,55 @@
+package ru.netology.domain;
+
+public class Radio {
+
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentStation;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setPrevStation(int currentStation) {
+        if (currentStation <= minStation) {
+            this.currentStation = maxStation;
+        } else this.currentStation = currentStation - 1;
+    }
+
+    public void setNextStation(int currentStation) {
+        if (currentStation >= maxStation) {
+            this.currentStation = minStation;
+        } else this.currentStation = currentStation + 1;
+    }
+
+    public int getVolume() {
+        return currentVolume;
+    }
+
+    public void setPrevVolume(int currentVolume) {
+        if (currentVolume <= minVolume) {
+            this.currentVolume = minVolume;
+        } else this.currentVolume = currentVolume - 1;
+    }
+
+    public void setNextVolume(int currentVolume) {
+        if (currentVolume >= maxVolume) {
+            this.currentVolume = maxVolume;
+        } else this.currentVolume = currentVolume + 1;
+    }
+
+}
+
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,122 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    @Test
+    public void shouldCurrentStationMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+       assertEquals(9, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldCurrentStationBeforeMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        assertEquals(8, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldCurrentStationAfterMax() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldCurrentStationMin() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        assertEquals(0, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldCurrentStationBeforeMin() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldCurrentStationAfterMin() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldPrevStationMin() {
+        Radio radio = new Radio();
+        radio.setPrevStation(0);
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldPrevStationBeforeMin() {
+        Radio radio = new Radio();
+        radio.setPrevStation(-1);
+        assertEquals(9, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldPrevStationAfterMin() {
+        Radio radio = new Radio();
+        radio.setPrevStation(1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldNextStationMax() {
+        Radio radio = new Radio();
+        radio.setNextStation(9);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldsetNextStationBeforeMax() {
+        Radio radio = new Radio();
+        radio.setNextStation(8);
+        assertEquals(9, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldNextStationAfterMax() {
+        Radio radio = new Radio();
+        radio.setNextStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+    @Test
+    public void shouldPrevVolumeMin() {
+        Radio radio = new Radio();
+        radio.setPrevVolume(0);
+        assertEquals(0, radio.getVolume());
+    }
+    @Test
+    public void shouldPrevVolumeBeforeMin() {
+        Radio radio = new Radio();
+        radio.setPrevVolume(-1);
+        assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    public void shouldPrevVolumeAfterMin() {
+        Radio radio = new Radio();
+        radio.setPrevVolume(1);
+        assertEquals(0, radio.getVolume());
+    }
+    @Test
+    public void shouldNextVolumeMax() {
+        Radio radio = new Radio();
+        radio.setNextVolume(10);
+        assertEquals(10, radio.getVolume());
+    }
+    @Test
+    public void shouldNextVolumeBeforeMax() {
+        Radio radio = new Radio();
+        radio.setNextVolume(9);
+        assertEquals(10, radio.getVolume());
+    }
+
+    @Test
+    public void shouldNextVolumeAfterMax() {
+        Radio radio = new Radio();
+        radio.setNextVolume(11);
+        assertEquals(10, radio.getVolume());
+    }
+}
